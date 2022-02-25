9202f5c68ce54c84bf51bf5aed3ae42a889f8683
OlMon <pawlowski.marco@gmail.com>
2022-02-25 16:31:36 +0100

    Updated README to include new functionality

--- consult-projectile.el
+++ consult-projectile.el
@@ -23,9 +23,11 @@
 
 ;;; Commentary:
 
-;; A multiview for displaying open buffers and files accociated with a project.
+;; A multiview for displaying open buffers, files and directories accociated with a project.
 ;; When no project is open in the current buffer display a list of known project.
 ;; and select a file from the selected project.
+;;
+;; Additionally seperate single source function are available.
 ;;
 ;; Just run the function `consult-projectile' and/or bind it to a hotkey.
 ;;
@@ -35,6 +37,11 @@
 ;; f - For project related files
 ;; p - For known projects
 ;; r - For project recent files
+;;
+;; The multiview includes initially buffers, files and known projects.  To include
+;; recent files and directires add `consult-projectile--source-projectile-dir' and/or
+;; `consult-projectile--source-projectile-recentf' to `consult-projectile-sources'.
+
 
 ;;; Code:
 
